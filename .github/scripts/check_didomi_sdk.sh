6499e7ed87b31b6c880aadfcec4d6e4b7a894ac8
Nicolas CHAIX <80761828+nicolas-chaix-didomi@users.noreply.github.com>
2024-09-04 19:55:19 +0200

    Update build config and scripts (#6)
    * update AGP
    
    * update gradle wrapper
    
    * update dependencies
    
    * fix scripts updating Didomi SDK dependency
    
    * update workflows actions

--- .github/scripts/check_didomi_sdk.sh
+++ .github/scripts/check_didomi_sdk.sh
@@ -12,21 +12,13 @@ if [[ ! $localVersion =~ ^[0-9]+.[0-9]+.[0-9]+$ ]]; then
   exit 1
 fi
 
-mavenVersion=""
-mavenUrl="$(curl -s 'https://repo.maven.apache.org/maven2/io/didomi/sdk/android/')"
-while read -r line; do
-  version="$(echo "$line" | sed -r 's/^.+href="([^"]+)".+$/\1/' | sed 's/.$//')"
-  if [[ $version =~ ^[0-9]+.[0-9]+.[0-9]+$ ]]; then
-    mavenVersion="$version"
-  fi
-done <<<"$mavenUrl"
-
-if [[ ! $mavenVersion =~ ^[0-9]+.[0-9]+.[0-9]+$ ]]; then
-  echo "Error while getting local android SDK version ($mavenVersion)"
+lastVersion=$(curl -s 'https://repo.maven.apache.org/maven2/io/didomi/sdk/android/maven-metadata.xml' | sed -ne '/release/{s/.*<release>\(.*\)<\/release>.*/\1/p;q;}')
+if [[ ! $lastVersion =~ ^[0-9]+.[0-9]+.[0-9]+$ ]]; then
+  echo "Error while getting latest android SDK version ($lastVersion)"
   exit 1
 fi
 
-if [[ $localVersion == "$mavenVersion" ]]; then
+if [[ $localVersion == "$lastVersion" ]]; then
   echo "Android SDK last version is $localVersion, no change"
   exit 0
 fi
